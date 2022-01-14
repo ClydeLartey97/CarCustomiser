3d4b695f60cc228786e95b71ae3382dd1696ddfb
ClydeLartey97 <lartey.d@etoncollege.org.uk>
2022-01-14 21:48:13 +0000

    Initial Commit

---
+++ CarCustomiser/CarCustomiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarCustomiserApp.swift
+//  CarCustomiser
+//
+//  Created by Clyde Lartey on 14/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct CarCustomiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
